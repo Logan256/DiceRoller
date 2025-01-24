5dbc3419bae6424bc411002131e41378ad34f05c
Logan256 <55640382+Logan256@users.noreply.github.com>
2025-01-24 14:48:16 -0700

    Fixed Siding Detecting with Presentation

--- GameViewController.swift
+++ GameViewController.swift
@@ -10,16 +10,23 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
-    var fallenDice: [SCNNode] = []
-    var landedSides: [String] = []
     
     var sceneView: SCNView!
     var scene: SCNScene!
     
+    var fallingDice: [SCNNode] = []
+    var sideList: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("AOINWDIOOAD")
         
+        setupScene()
+        setupWorldBoundary(for: scene)
+    }
+    
+    func setupScene() {
+        
+        print("Starting Board")
         // Load the scene from ar.scnassets
         scene = SCNScene(named: "art.scnassets/MainBoard.scn")!
         
@@ -31,12 +38,12 @@ class GameViewController: UIViewController {
         // Add a gesture tap recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         sceneView.addGestureRecognizer(tapGesture)
-        
-        // Create a world boundary to the physics world
-        setupWorldBoundary(for: scene)
-        
-        // Set delegate
+    
+        // Set render delegate may not need
         sceneView.delegate = self
+        
+        // Set up physics world delegate
+        scene?.physicsWorld.contactDelegate = self
     }
     
     func setupWorldBoundary(for scene: SCNScene) {
@@ -86,30 +93,34 @@ class GameViewController: UIViewController {
         let cameraNode = scene.rootNode.childNode(withName: "camera", recursively: true)!
         
         // Load die file
-        let boxScene = SCNScene(named: "art.scnassets/dTest.scn")!
+        let dieScene = SCNScene(named: "art.scnassets/dTest.scn")!
         
         // Get the root node of the die file
-        let boxNode = boxScene.rootNode.childNodes.first!
+        let dieNode = dieScene.rootNode.childNodes.first!
+//        
+//        let dieNode = SCNNode(geometry: SCNSphere(radius: 0.1))
+//        dieNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+//        dieNode.name = "ball"
         
         // Set the position of the die at the camera
-        boxNode.position = SCNVector3(cameraNode.position.x, cameraNode.position.y, cameraNode.position.z)
-        
+        dieNode.position = SCNVector3(cameraNode.position.x, cameraNode.position.y, cameraNode.position.z)
+
         // Apply a random force
         let randomX = Float.random(in: -0.2...0.2)
         let randomY = Float.random(in: -0.2...0.2)
         let randomZ = Float.random(in: -0.2...0.2)
-        boxNode.physicsBody?.applyForce(SCNVector3(x: randomX, y: randomY - 0.02, z: randomZ), asImpulse: true)
+        dieNode.physicsBody?.applyForce(SCNVector3(x: randomX, y: randomY - 0.02, z: randomZ), asImpulse: true)
         
         // Apply a random spin (torque)
         let randomTorqueX = Float.random(in: -0.1...0.1)
         let randomTorqueY = Float.random(in: -0.1...0.1)
         let randomTorqueZ = Float.random(in: -0.1...0.1)
-        boxNode.physicsBody?.applyTorque(SCNVector4(x: randomTorqueX, y: randomTorqueY, z: randomTorqueZ, w: 1.0), asImpulse: true)
+        dieNode.physicsBody?.applyTorque(SCNVector4(x: randomTorqueX, y: randomTorqueY, z: randomTorqueZ, w: 1.0), asImpulse: true)
         
-        print(boxNode.worldPosition)
+        print(dieNode.worldPosition)
         // Add the die to the main scene
-        scene.rootNode.addChildNode(boxNode)
-        fallenDice.append(boxNode)
+        scene.rootNode.addChildNode(dieNode)
+        fallingDice.append(dieNode)
     }
     
     override var prefersStatusBarHidden: Bool {
@@ -127,34 +138,23 @@ class GameViewController: UIViewController {
     // Function to calculate which side is closest to the ground (assuming y = 0 is the ground)
     func calculateClosestSide(for dieNode: SCNNode) -> SCNNode? {
         print("Calculating Side")
-        
         let markerNodes = dieNode.childNodes
-      
-        var closestMarker: SCNNode?
-        var minDistance: Float = Float.greatestFiniteMagnitude
         
-//        let floorNode = scene.rootNode.childNode(withName: "floor", recursively: true)!
+        var furthestMarker: SCNNode?
+        var maxDistance: Float = 0.0
 
-//        print(floorNode.worldPosition)
-        print(dieNode.worldTransform)
-
-        
         for marker in markerNodes {
-            // Calculate distance to the ground (y = 0). We care about the absolute y position
-            print(marker.worldPosition)
-            let distance = marker.worldPosition.y // Assuming marker at y = 0 is closest to the ground
-            
-            // We check for the side that's closest to 0 in terms of y
-            if distance < minDistance {
-                closestMarker = marker
-                minDistance = distance
+            let distance = marker.presentation.worldPosition.y // Assuming marker at y = 0 is closest to the ground
+
+            // check for the side that's furthest from 0 in terms of y
+            if distance > maxDistance {
+                furthestMarker = marker
+                maxDistance = distance
             }
         }
-        
-        // If you want more clarity, let's print the distances for debugging:
-        print("Closest marker: \(closestMarker?.name ?? "None"), distance: \(minDistance)")
-        
-        return closestMarker
+
+        print("Furthest marker: \(furthestMarker?.name ?? "None"), distance: \(maxDistance)")
+        return furthestMarker
     }
 
     
@@ -167,22 +167,27 @@ class GameViewController: UIViewController {
 
 extension GameViewController: SCNSceneRendererDelegate {
     func renderer(_ renderer: any SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        // Check each fallen die and find the closest side when it has stopped moving
-//        print("Checking render")
-        for boxNode in fallenDice {
-            if let boxPhysicsBody = boxNode.physicsBody {
-                // Check if the velocity is approximately zero
-                if isVelocityZero(boxPhysicsBody.velocity) {
-                    // Die has stopped moving, now check for the closest side
-                    if let closestMarker = calculateClosestSide(for: boxNode) {
-                        print("Closest side for die \(boxNode.name ?? "Unnamed") is \(closestMarker.name ?? "No side")")
-                        let index = fallenDice.firstIndex(of: boxNode)!
-                        fallenDice.remove(at: index)
-                        landedSides.append(closestMarker.name!)
-                        print(landedSides)
-                    }
+        for dieNode in fallingDice {
+            if dieNode.physicsBody?.isResting == true {
+                let die = dieNode.presentation
+                print("Die Presentation")
+                print(die.position)
+                if let side = calculateClosestSide(for: dieNode) {
+                    print(side.name ?? "You fucked up")
+                    sideList.append(side.name!)
+                }
+                
+                // remove die notde
+                if let index = fallingDice.firstIndex(of: dieNode) {
+                    fallingDice.remove(at: index)
                 }
             }
         }
+    }
+}
+
+extension GameViewController: SCNPhysicsContactDelegate {
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
+        // Code
     }
 }
